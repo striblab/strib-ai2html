13c0282209efdaa0b6e996bdc91a742b963540c4
Alan Palazzolo <github@zzolo.org>
2018-09-14 12:40:22 -0500

    Furthering.

--- bin/commands/generator.js
+++ bin/commands/generator.js
@@ -4,6 +4,10 @@
 
 // Dependencies
 const os = require('os');
+const inquirer = require('inquirer');
+const _ = require('lodash');
+const errors = require('../../lib/errors.js');
+const { CLIOutput } = require('../../lib/cli-output.js');
 const { generate } = require('../../lib/generate.js');
 const debug = require('debug')('strib-ai2html:command:generate');
 
@@ -28,11 +32,61 @@ exports.builder = yargs => {
     default: 'v0.77.0'
   });
 
+  yargs.options('font-check', {
+    describe:
+      'Check for font or not.  Do not skip font check unless you know what you are doing.',
+    type: 'boolean',
+    default: true
+  });
+
   return yargs;
 };
 
 // Main function
-exports.handler = async argv => {
-  // TODO
-  await generate(argv);
-};
+async function main(argv, noTitle = false) {
+  // Setupt output
+  let output = new CLIOutput();
+  if (!noTitle) {
+    output.intro('Generate');
+  }
+
+  try {
+    output.out('Generating ...');
+    await generate(argv);
+    output.done();
+  }
+  catch (e) {
+    // Handle font check error
+    if (e.id === errors.fontCheck) {
+      output.error(`${e.message}
+        Unable to find the following fonts: ${_.map(
+    _.filter(e.data, set => !set.found),
+    'name'
+  ).join(', ')}`);
+
+      // Prompt to override
+      return inquirer
+        .prompt({
+          name: 'skipFontCheck',
+          message: `${
+            output.indent
+          }Do you wish to continue anyway?  This may lead to fonts not exported correctly when using ai2html`,
+          type: 'confirm',
+          default: false
+        })
+        .then(answers => {
+          if (answers.skipFontCheck) {
+            argv.fontCheck = false;
+            return main(argv, true);
+          }
+
+          output.exit('Did not generate ai2html.');
+        });
+    }
+
+    output.error(e);
+    output.exit('Did not generate ai2html.');
+  }
+}
+
+exports.handler = main;
